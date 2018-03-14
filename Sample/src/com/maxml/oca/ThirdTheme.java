0a6c547f070a41e295255464f84e8b7bff792e78
mliashenko <maxml@inbox.ru>
2018-03-14 11:22:54 +0200

    library + third file start

--- Sample/src/com/maxml/oca/ThirdTheme.java
+++ Sample/src/com/maxml/oca/ThirdTheme.java
@@ -3,17 +3,11 @@ package com.maxml.oca;
 public class ThirdTheme {
 
     public static void main(String[] Args) {
-        System.out.println("Hello");
-
-        exValueOf();
+//        exValueOf();
     }
 
     private static void exValueOf() {
         String buff = "a";
-        try {
-            System.out.println(Integer.valueOf(buff));
-        } catch (NumberFormatException ex) {
-            
-        }
+        System.out.println(Integer.valueOf(buff));
     }
 }
